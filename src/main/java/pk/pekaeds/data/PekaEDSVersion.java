6bd56542d384416a814cef9d0ec693358f89d6e7
Detea <deta42@web.de>
2023-02-20 23:53:38 +0100

    Getting ready for release 0.4 beta

--- src/main/java/pk/pekaeds/data/PekaEDSVersion.java
+++ src/main/java/pk/pekaeds/data/PekaEDSVersion.java
@@ -1,5 +1,5 @@
 package pk.pekaeds.data;
 
 public class PekaEDSVersion {
-    public static final String VERSION_STRING = "Beta 0.3";
+    public static final String VERSION_STRING = "Beta 0.4";
 }
