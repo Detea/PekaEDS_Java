2c46bec04ccb6e528deff8d945b22205c1da2571
Detea <deta42@web.de>
2023-02-20 22:19:06 +0100

    Updated version strings to 0.3-beta, ready for release

--- src/main/java/pk/pekaeds/data/PekaEDSVersion.java
+++ src/main/java/pk/pekaeds/data/PekaEDSVersion.java
@@ -1,5 +1,5 @@
 package pk.pekaeds.data;
 
 public class PekaEDSVersion {
-    public static final String VERSION_STRING = "Beta 0.2.1";
+    public static final String VERSION_STRING = "Beta 0.3";
 }
